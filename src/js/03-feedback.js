3cf338a81b52de0f1c9db6bc20bf30c2dc22c2aa
LidiyaYem <102388444+LidiyaYem@users.noreply.github.com>
2023-02-20 23:24:58 +0200

    update-task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', throttle (onTextareaInput, 500));
+
+refs.form.addEventListener('input', e => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+
+    formData[e.target.name] = e.target.value;
+
+    console.log(formData);
+})
+
+populateTextarea();
+
+function onFormSubmit(e) {
+    e.preventDefault();
+
+    e.currentTarget.reset();
+
+    localStorage.removeItem('STORAGE_KEY');
+
+    console.log(formData);
+}
+
+function onTextareaInput(e) {
+    const message = e.target.value;
+
+    localStorage.setItem('STORAGE_KEY', JSON.stringify(formData));
+}
+
+function populateTextarea(e) {
+    const savedMessage = localStorage.getItem('STORAGE_KEY');
+
+    const parsedMessage = JSON.parse(savedMessage);
+
+
+
+    if (parsedMessage) {
+        console.log(parsedMessage);
+        refs.textarea.value = parsedMessage; 
+    }
+
+}
